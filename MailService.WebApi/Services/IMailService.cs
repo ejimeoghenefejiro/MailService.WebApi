ff76e7ab9c3be08fb59bba5a7634fcc3061936ef
oghenefejiro <ejimeoghenefejiro@yahoo.com>
2021-04-27 12:01:41 +0100

    Complete Code

---
+++ MailService.WebApi/Services/IMailService.cs
@@ -0,0 +1,14 @@
+﻿using MailService.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailService.WebApi.Services
+{
+public interface IMailService
+{
+    Task SendEmailAsync(MailRequest mailRequest);
+    Task SendWelcomeEmailAsync(WelcomeRequest request);
+}
+}
